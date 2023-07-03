84aba1e8f2be08a5ea86d5f8a94abf2f47cdd167
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-03 13:32:08 -0500

    login and book catering links added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,6 +11,11 @@ function NavBar() {
           <li className='p_opensans'><a href='#home'>Awards</a></li>
           <li className='p_opensans'><a href='#home'>Contact</a></li>
         </ul>
+        <div className='app__navbar-login'>
+          <a href='#login' className='p__opensans'>Log In / Registration</a>
+          <div />
+          <a href='/' className='p__opensans'>Book Catering</a>
+        </div>
       </div>
     </nav>
   )
