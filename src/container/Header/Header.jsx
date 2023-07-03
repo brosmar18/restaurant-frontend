e5762748c8384852a17b5948a3d52e97676528a7
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-03 18:10:43 -0500

    title added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
     <header className='app__header app__wrapper section__padding' id='home'>
         <div className='app__wrapper_info'>
           <SubHeading />
+          <h1 className='app__header-h1'>The Key to Fine Dining</h1>
         </div>
 
         <div className='app__wrapper_img'>
