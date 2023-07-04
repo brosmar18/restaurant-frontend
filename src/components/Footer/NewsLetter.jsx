5fca2862a203b2586899e8a309c3091d83d555c8
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 12:49:10 -0500

    Subheading added with title and text

--- src/components/Footer/NewsLetter.jsx
+++ src/components/Footer/NewsLetter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SubHeading from '../SubHeading/SubHeading'
 
 function NewsLetter() {
   return (
-    <div>
-      NewsLetter
+    <div className='app__newsletter'>
+        <div className='app__newsletter-heading'>
+            <SubHeading title='Newsletter' />
+            <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+            <p className='p__opensans'>And never miss latest Updates!</p>
+        </div>
     </div>
   )
 }
