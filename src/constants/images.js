7f2108bd84b4bf3f018d23ba1b9812ba16ba2501
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 09:17:54 -0500

    Image added

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,11 @@
 import gericht from '../assets/gericht.png';
 import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
+import G from '../assets/G.png';
 
 export default {
     gericht,
     spoon,
-    welcome
+    welcome,
+    G
 };
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {images} from '../../constants';
 
 function AboutUs() {
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+    <section className='app__aboutus app__bg flex__center section__padding' id='about'>
+      <div className='app__aboutus-overlay flex__center'>
+        <img src={images.G} alt='G' />
+      </div>
+    </section>
   )
 }
 
