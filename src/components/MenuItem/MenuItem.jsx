b50d23f1fe0166c8221a861bf2e33be51de05007
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 10:24:39 -0500

    dash, price and sub sections added

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -7,6 +7,14 @@ function MenuItem({title, price, tags}) {
             <div className='app__menuitem-name'>
                 <p className='p__cormorant' style={{color: '#DCCA87'}}>{title}</p>
             </div>
+            <div className='app__menuitem-dash' />
+            <div className='app__menuitem-price'>
+                <p className='p__cormorant'>{price}</p>
+            </div>
+        </div>
+
+        <div className='app__menuitem-sub'>
+            <p className='p__opensans' style={{color: '#AAA'}}>{tags}</p>
         </div>
     </div>
   )
