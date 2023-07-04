a6bc8c201f09c0712f73fd3ccefba5355e631640
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 18:12:57 -0500

    unused images removed

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,16 +1,11 @@
 import logo from '../assets/logo.png';
 import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
-import G from '../assets/G.png';
-import knife from '../assets/knife.png';
 import award01 from '../assets/award01.png';
 import award02 from '../assets/award02.png';
 import award03 from '../assets/award03.png';
 import award05 from '../assets/award05.png';
 import menu from '../assets/menu.png';
-import chef from '../assets/chef.png';
-import quote from '../assets/quote.png';
-import sign from '../assets/sign.png';
 import laurels from '../assets/laurels.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
@@ -18,7 +13,6 @@ import gallery03 from '../assets/gallery03.png';
 import gallery04 from '../assets/gallery04.png';
 import findus from '../assets/findus.png';
 import mug from '../assets/mug.png';
-import mug2 from '../assets/mug2.png';
 import spatula from '../assets/spatula.png';
 
 
@@ -26,16 +20,11 @@ export default {
     logo,
     spoon,
     welcome,
-    G,
-    knife,
     award01,
     award02,
     award03,
     award05,
     menu,
-    chef,
-    quote,
-    sign,
     laurels,
     gallery01,
     gallery02,
@@ -43,6 +32,5 @@ export default {
     gallery04,
     findus,
     mug,
-    mug2,
     spatula
 };
