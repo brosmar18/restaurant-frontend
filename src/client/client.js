b8bdd5f2b2eb48a439822f14e023739492161a03
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 15:02:02 -0500

    client created

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,11 @@
 import { createClient } from "@sanity/client";
 import { ImageUrlBuilder } from "@sanity/image-url";
 
+const client = createClient({
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2023-05-26',
+    useCdn: true,
+    token: process.env.REACT_APP_SANITY_TOKEN,
+    ignoreBrowserTokenWarning: true
+});
