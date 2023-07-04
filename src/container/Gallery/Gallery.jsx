a639d29c794a284f21ef23c08251f18e0eebd78b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 12:06:57 -0500

    scroll logic added

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
@@ -6,6 +6,19 @@ import { images } from '../../constants';
 import './Gallery.css';
 
 function Gallery() {
+
+  const scrollRef = useRef(null);
+  const scroll = (direction) => {
+    const {current} = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollRef -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  
   return (
     <div>
       <h1>Gallery</h1>
