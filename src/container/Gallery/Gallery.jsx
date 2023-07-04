5b8256cf8ec01c33c53d82004d22c0f7233a5e85
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 12:18:11 -0500

    images setup for export, rendered

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
@@ -9,7 +9,7 @@ function Gallery() {
 
   const scrollRef = useRef(null);
   const scroll = (direction) => {
-    const {current} = scrollRef;
+    const { current } = scrollRef;
 
     if (direction === 'left') {
       current.scrollRef -= 300;
@@ -26,9 +26,14 @@ function Gallery() {
         <h1 className='headtext__cormorant'>Photo Gallery</h1>
         <button type='button' className='custom__button'>View More</button>
       </div>
-      <div className='app__gallery-images'>
-        
-      </div>
+      <div className="app__gallery-images_container" ref={scrollRef}>
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
     </section>
   )
 }
