1dd6e9b171feb66be5fddb52e8c960a4d7ff1858
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 13:01:41 -0500

    logo section added

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -19,6 +19,17 @@ function Footer() {
         <p className="p__opensans">theemail@email.com</p>
         </div>
       </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
     </section>
   )
 }
