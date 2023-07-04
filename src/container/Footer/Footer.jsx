783b596082a8e92a5a9bb20c3372482f8eba3f2e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 13:00:37 -0500

    contact text added

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -7,6 +7,15 @@ function Footer() {
     <section className='app__footer section__padding' id='login'>
       <FooterOverlay />
       <NewsLetter />
+
+      <div className='app__footer-links'>
+        <div className='app__footer-links_contact'>
+          <h1 className='app__footer-headtext'>Contact Us</h1>
+          <p className="p__opensans">838 12th AVE SW, Cedar Rapids, Iowa 52404</p>
+        <p className="p__opensans">(319) 999-9999</p>
+        <p className="p__opensans">theemail@email.com</p>
+        </div>
+      </div>
     </section>
   )
 }
