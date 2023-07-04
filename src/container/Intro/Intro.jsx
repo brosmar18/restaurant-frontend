063f706c971fa78c6455148a8fdcea1a75afa82b
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 11:23:38 -0500

    hooks added

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
 function Intro() {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+  
   return (
     <div>
       <h1>Intro</h1>
