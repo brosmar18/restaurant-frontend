72a21ea12847718c73c843c50f80894235dcdddd
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 10:27:20 -0500

    MenuItem and data imported to menu

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import NavBar from './NavBar/NavBar';
 import SubHeading from './SubHeading/SubHeading';
+import MenuItem from './MenuItem/MenuItem';
 
 export {
     NavBar,
-    SubHeading
+    SubHeading,
+    MenuItem
 };
--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import {SubHeading} from '../../components';
-import {images} from '../../constants';
+import {SubHeading, MenuItem} from '../../components';
+import {images, data} from '../../constants';
 import './Menu.css';
 
 function Menu() {
