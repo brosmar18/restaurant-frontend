36b323acb4d1497f352741a777783c983d1d9241
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 11:25:40 -0500

    meal video added

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,14 +1,24 @@
 import React, {useState, useRef} from 'react';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
 
 function Intro() {
   const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
-  
+
   return (
-    <div>
-      <h1>Intro</h1>
-    </div>
+    <section className='app__video'>
+      <video
+        ref={vidRef}
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+
+    </section>
   )
 }
 
