738346b3634f0664cfd4102843351ef0c37cdcfa
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 12:56:36 -0500

    FooterOverlay styling divs added

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './FooterOverlay.css';
 
 function FooterOverlay() {
   return (
-    <div>
-      Overlay
+    <div className='app__footerOverlay'>
+      <div className='app__footerOverlay-black' />
+      <div className='app__footerOverlay-img app__bg' />
     </div>
   )
 }
