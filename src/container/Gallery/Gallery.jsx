35874ac865a34cd83dff5b913b5a1498e9c36934
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 12:05:19 -0500

    imports added

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Gallery.css';
 
 function Gallery() {
   return (
