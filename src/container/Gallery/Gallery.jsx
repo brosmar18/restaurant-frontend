0fd4e057ed073e532f8d6255aa41b6579bc1371e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-04 12:19:02 -0500

    icons added

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -20,13 +20,15 @@ function Gallery() {
 
 
   return (
-    <section className='app__gallery flex__center'>
-      <div className='app__gallery-content'>
+    <section className="app__gallery flex__center">
+      <div className="app__gallery-content">
         <SubHeading title="Instagram" />
-        <h1 className='headtext__cormorant'>Photo Gallery</h1>
-        <button type='button' className='custom__button'>View More</button>
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
       </div>
-      <div className="app__gallery-images_container" ref={scrollRef}>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
           {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
             <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
               <img src={image} alt="gallery_image" />
@@ -34,8 +36,13 @@ function Gallery() {
             </div>
           ))}
         </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Gallery
